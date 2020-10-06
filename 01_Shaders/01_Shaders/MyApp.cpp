f2de3c848af3fb627313216764f51702a2ae0610
fiko.robert <fiko.robert@gmail.com>
2020-10-06 10:13:18 +0200

    Initial commit

---
+++ 01_Shaders/01_Shaders/MyApp.cpp
@@ -0,0 +1,149 @@
+#include "MyApp.h"
+#include "GLUtils.hpp"
+
+#include <math.h>
+
+CMyApp::CMyApp(void)
+{
+}
+
+
+CMyApp::~CMyApp(void)
+{
+}
+
+bool CMyApp::Init()
+{
+	// törlési szín legyen kékes
+	glClearColor(0.125f, 0.25f, 0.5f, 1.0f);
+
+	glEnable(GL_CULL_FACE); // kapcsoljuk be a hatrafele nezo lapok eldobasat
+	glEnable(GL_DEPTH_TEST); // mélységi teszt bekapcsolása (takarás)
+
+	//
+	// shaderek betöltése
+	//
+	GLuint vs_ID = loadShader(GL_VERTEX_SHADER,		"myVert.vert");
+	GLuint fs_ID = loadShader(GL_FRAGMENT_SHADER,	"myFrag.frag");
+
+	// a shadereket tároló program létrehozása
+	m_programID = glCreateProgram();
+
+	// adjuk hozzá a programhoz a shadereket
+	glAttachShader(m_programID, vs_ID);
+	glAttachShader(m_programID, fs_ID);
+
+	// illesszük össze a shadereket (kimenõ-bemenõ változók összerendelése stb.)
+	glLinkProgram(m_programID);
+
+	// linkeles ellenorzese
+	GLint infoLogLength = 0, result = 0;
+
+	glGetProgramiv(m_programID, GL_LINK_STATUS, &result);
+	glGetProgramiv(m_programID, GL_INFO_LOG_LENGTH, &infoLogLength);
+	if ( GL_FALSE == result )
+	{
+		std::vector<char> VertexShaderErrorMessage(infoLogLength);
+		glGetProgramInfoLog(m_programID, infoLogLength, NULL, &VertexShaderErrorMessage[0]);
+
+		std::cerr << &VertexShaderErrorMessage[0];
+	}
+
+	// már nincs ezekre szükség
+	glDeleteShader( vs_ID );
+	glDeleteShader( fs_ID );
+
+	return true;
+}
+
+void CMyApp::Clean()
+{
+	glDeleteProgram( m_programID );
+}
+
+void CMyApp::Update()
+{
+}
+
+
+void CMyApp::Render()
+{
+	// töröljük a frampuffert (GL_COLOR_BUFFER_BIT) és a mélységi Z puffert (GL_DEPTH_BUFFER_BIT)
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// shader bekapcsolasa
+	glUseProgram( m_programID );
+	glUniform2fv(glGetUniformLocation(m_programID, "move"), 1, move);
+	glUniform1f(glGetUniformLocation(m_programID, "zoom"), zoom);
+
+	// kirajzolás: https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/glDrawArrays.xhtml
+	glDrawArrays(	GL_TRIANGLE_STRIP,	// primitív típusa; amikkel mi foglalkozunk: GL_POINTS, GL_LINE_STRIP, GL_LINES, GL_TRIANGLE_STRIP, GL_TRIANGLE_FAN, GL_TRIANGLES
+					0,					// ha van tároló amiben a kirajzolandó geometriák csúcspontjait tároljuk, akkor annak hányadik csúcspontjától rajzoljunk - most nincs ilyen, 
+										// csak arra használjuk, hogy a gl_VertexID számláló a shader-ben melyik számról induljon, azaz most nulláról
+					4);					// hány csúcspontot használjunk a primitívek kirajzolására - most: gl_VertexID számláló 0-tól indul és 3-ig megy, azaz összesen 4x fut le a vertex shader
+
+	// shader kikapcsolasa
+	glUseProgram( 0 );
+}
+
+void CMyApp::KeyboardDown(SDL_KeyboardEvent& key)
+{
+
+	switch (key.keysym.sym) {
+	case SDLK_RIGHT: 
+		move[0] += 0.1f;
+		break;
+
+	case SDLK_LEFT:
+		move[0] -= 0.1f;
+		break;
+	
+
+	case SDLK_UP:
+		move[1] += 0.1f;
+		break;
+
+	case SDLK_DOWN:
+		move[1] -= 0.1f;
+		break;
+
+	case SDLK_a:
+		zoom *= .95f;
+		break;
+
+	case SDLK_s:
+		zoom /= .95f;
+		break;
+
+	}
+	std::cout << move[0] << " " << move[1];
+
+	zoom = glm::clamp(zoom, .000005f, 10.0f);
+}
+
+void CMyApp::KeyboardUp(SDL_KeyboardEvent& key)
+{
+}
+
+void CMyApp::MouseMove(SDL_MouseMotionEvent& mouse)
+{
+
+}
+
+void CMyApp::MouseDown(SDL_MouseButtonEvent& mouse)
+{
+}
+
+void CMyApp::MouseUp(SDL_MouseButtonEvent& mouse)
+{
+}
+
+void CMyApp::MouseWheel(SDL_MouseWheelEvent& wheel)
+{
+}
+
+// a két paraméterbe az új ablakméret szélessége (_w) és magassága (_h) található
+void CMyApp::Resize(int _w, int _h)
+{
+	glViewport(0, 0, _w, _h );
+}
---
+++ 02_SDLEvents/02_SDLEvents/main.cpp
@@ -0,0 +1,195 @@
+#include <SDL.h>
+#include <iostream>
+
+void exitProgram()
+{
+	std::cout << "Kilépéshez nyomj meg egy billentyût..." << std::endl;
+	std::cin.get();
+}
+
+int main( int argc, char* args[] )
+{
+	atexit( exitProgram );
+	//
+	// 1. lépés: inicializáljuk az SDL-t
+	//
+
+	// a grafikus alrendszert kapcsoljuk csak be, ha gond van, akkor jelezzük és lépjün ki
+	if ( SDL_Init( SDL_INIT_VIDEO ) == -1 )
+	{
+		// irjuk ki a hibat es terminaljon a program
+		std::cout << "[SDL indítása]Hiba az SDL inicializálása közben: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+			
+	//
+	// 2. lépés: hozzuk létre az ablakot, amire rajzolni fogunk
+	//
+
+	SDL_Window *win = nullptr;
+    win = SDL_CreateWindow( "Hello SDL!",				// az ablak fejléce
+							100,						// az ablak bal-felsõ sarkának kezdeti X koordinátája
+							100,						// az ablak bal-felsõ sarkának kezdeti Y koordinátája
+							640,						// ablak szélessége
+							480,						// és magassága
+							SDL_WINDOW_SHOWN);			// megjelenítési tulajdonságok
+
+	// ha nem sikerült létrehozni az ablakot, akkor írjuk ki a hibát, amit kaptunk és lépjünk ki
+    if (win == nullptr)
+	{
+		std::cout << "[Ablak létrehozása]Hiba az SDL inicializálása közben: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+	//
+	// 3. lépés: hozzunk létre egy renderelõt, rajzolót
+	//
+
+    SDL_Renderer *ren = nullptr;
+    ren = SDL_CreateRenderer(	win, // melyik ablakhoz rendeljük hozzá a renderert
+								-1,  // melyik indexú renderert inicializáljuka
+									 // a -1 a harmadik paraméterben meghatározott igényeinknek megfelelõ elsõ renderelõt jelenti
+								SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);	// az igényeink, azaz
+																						// hardveresen gyorsított és vsync-et beváró
+    if (ren == nullptr)
+	{
+        std::cout << "[Renderer létrehozása]Hiba az SDL inicializálása közben: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+	//
+	// 4. lépés: indítsuk el a fõ üzenetfeldolgozó ciklust
+	// 
+
+	// véget kell-e érjen a program futása?
+	bool quit = false;
+	// feldolgozandó üzenet ide kerül
+	SDL_Event ev;
+	// egér X és Y koordinátái
+	Sint32 mouseX = 0, mouseY = 0;
+
+	float cursorColor[] = { 255, 255, 255 };
+
+	while (!quit)
+	{
+		// amíg van feldolgozandó üzenet dolgozzuk fel mindet:
+		while ( SDL_PollEvent(&ev) )
+		{
+			switch (ev.type)
+			{
+			case SDL_QUIT:
+				quit = true;
+				break;
+			case SDL_KEYDOWN:
+				if ( ev.key.keysym.sym == SDLK_ESCAPE )
+					quit = true;
+
+				if (ev.key.keysym.sym == SDLK_SPACE) {
+					cursorColor[0] = rand() % 256; 
+					cursorColor[1] = rand() % 256;
+					cursorColor[2] = rand() % 256;
+				}
+
+				break;
+			case SDL_MOUSEMOTION:
+				mouseX = ev.motion.x;
+				mouseY = ev.motion.y;
+				break;
+			case SDL_MOUSEBUTTONUP:
+				// egérgomb felengedésének eseménye; a felengedett gomb a ev.button.button -ban található
+				// a lehetséges gombok a következõek: SDL_BUTTON_LEFT, SDL_BUTTON_MIDDLE, 
+				//		SDL_BUTTON_RIGHT, SDL_BUTTON_WHEELUP, SDL_BUTTON_WHEELDOWN
+
+				if (ev.button.button == SDL_BUTTON_RIGHT) {
+					cursorColor[0] = 0;
+					cursorColor[1] = 0;
+					cursorColor[2] = 255;
+				}
+
+				else if (ev.button.button == SDL_BUTTON_LEFT) {
+					cursorColor[0] = 255;
+					cursorColor[1] = 0;
+					cursorColor[2] = 0;
+				}
+
+				break;
+			}
+		}
+
+		// töröljük a hátteret fehérre
+		SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
+		SDL_RenderClear(ren);
+
+		// aktuális rajzolási szín legyen zöld és rajzoljunk ki egy vonalat
+		SDL_SetRenderDrawColor(	ren,	// renderer címe, aminek a rajzolási színét be akarjuk állítani
+								0,		// piros
+								255,	// zöld
+								0,		// kék
+								255);	// átlátszatlanság
+
+		SDL_RenderDrawLine(	ren,	// renderer címe, ahová vonalat akarunk rajzolni
+							0, 0, // vonal kezdõpontjának (x,y) koordinátái
+							mouseX, mouseY);// vonal végpontjának (x,y) koordinátái
+		
+		// definiáljunk egy (mouseX, mouseY) középpontó, tengelyekkel párhuzamos oldalú
+		// 20x20-as négyzetet:
+		SDL_Rect cursor_rect;
+		cursor_rect.x = mouseX - 10;
+		cursor_rect.y = mouseY - 10;
+		cursor_rect.w = 20 * abs(sin(SDL_GetTicks() / 1000) * M_PI);
+		cursor_rect.h = 20 * abs(sin(SDL_GetTicks() / 1000) * M_PI);
+		// legyen a kitöltési szín piros
+		SDL_SetRenderDrawColor( ren, cursorColor[0], cursorColor[1], cursorColor[2], 255 );
+		SDL_RenderFillRect( ren, &cursor_rect);
+
+		// 1. feladat: az eltelt idõ függvényében periodikusan nõjjön és csökkenjen
+		//    az egérmutató középpontjával kirajzolt négyszög
+
+		std::cout << SDL_GetTicks() << std::endl;
+
+
+		//SDL_GetTicks() visszaadja az eltelt idõt
+	
+
+
+
+		// 2. feladat: ha a user a bal egérgombot nyomja meg akkor a téglalap színe váltson pirosra,
+		//    ha a jobb egérgombot, akkor kékre
+
+
+
+
+		// 3. feladat: rajzolj ki egy 50 sugarú körvonalat az egérmutató köré!
+		// segítség: használd a SDL_RenderDrawLines()-t
+
+		SDL_Point points[30];
+		int r = 100;
+
+		for (int i = 0; i < 30; i++)
+		{
+			float a = M_PI * 2 / 29 * i;
+			points[i] = { mouseX + (int)(r * cosf(a)),  mouseY + (int)(r * sinf(a)) };
+		}
+
+		SDL_RenderDrawLines(ren, points, 30);
+
+
+
+
+		// jelenítsük meg a backbuffer tartalmát
+		SDL_RenderPresent(ren);
+	}
+
+
+
+	//
+	// 4. lépés: lépjünk ki
+	// 
+
+	SDL_DestroyRenderer( ren );
+	SDL_DestroyWindow( win );
+
+	SDL_Quit();
+
+	return 0;
+}
